2e4b855a456e70a2c7cfe1a97195f2b4dc8e67b8
goncalo barbosa <a104438@alunos.uminho.pt>
2026-04-14 13:23:50 +0100

    fix: sync correct folder names and remove duplicates

--- LI3/LI3/2425-G16/trabalho-pratico/include/controller.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-void run(char *argv[]);
-void run_testes(char *argv[]);
-void run_interactive();
-
-#endif
--- LI3/LI3/2425-G16/trabalho-pratico/include/output.h
+++
@@ -1,9 +0,0 @@
-#ifndef OUTPUT_H
-#define OUTPUT_H
-
-#include "joint.h"
-
-void escreve_querie(char *resultado,int contador);
-
-
-#endif
